fec5332958ba8732fe9e110f84c09eb50e98632e
Szuszkiewicz <yck920601@gmail.com>
2026-05-18 18:41:16 +0800

    Nacos

--- peter-article/peter-article-main/src/main/java/com/Peter/ArticleApplication.java
+++ peter-article/peter-article-main/src/main/java/com/Peter/ArticleApplication.java
@@ -2,11 +2,13 @@ package com.Peter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 文章启动类
  *
  */
+@EnableDiscoveryClient//注册服务
 @SpringBootApplication(scanBasePackages ={"com.Peter"})
 public class ArticleApplication {
     public static void main(String[] args) {
