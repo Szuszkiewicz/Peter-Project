b9a6feb5ad90c2e6b1968dbaee776185067ab758
Szuszkiewicz <yck920601@gmail.com>
2026-05-24 12:20:14 +0800

    文章按阅读量排行 阅读量增加

--- peter-article/peter-article-orm/src/main/java/com/Peter/mapper/ArticleMapper.java
+++ peter-article/peter-article-orm/src/main/java/com/Peter/mapper/ArticleMapper.java
@@ -1,6 +1,8 @@
 package com.Peter.mapper;
 
+import com.Peter.entity.Article;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 
 import java.util.List;
@@ -14,4 +16,8 @@ public interface ArticleMapper {
     int addCollectsCount(Long  id);
 
     int subtractCollectsCount(Long  id);
+
+    int addViewsCount(Long  id);
+
+    List<Article> selectAll(@Param("article") Article article, @Param("distinct") Boolean distinct);
 }
